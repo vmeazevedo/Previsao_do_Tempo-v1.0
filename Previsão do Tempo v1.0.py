4dca5215ee4a7f60cb3c0c7bb252038bbe3cda6a
Vinícius Azevedo <vmeazevedo@gmail.com>
2020-04-01 13:13:55 -0300

    Update Previsão do Tempo v1.0.py

--- Previsão do Tempo v1.0.py
+++ Previsão do Tempo v1.0.py
@@ -2,7 +2,7 @@ import requests
 import json
 import pprint
 
-accuweatherAPIKey = 'ZJYXMjCTnyAApLhwdP7Kdt2EaEw9tEnP'
+accuweatherAPIKey = 'DIGITE_A_API_KEY_DO_ACUWEATHER'
 
 r = requests.get('http://www.geoplugin.net/json.gp')
 
